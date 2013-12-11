3d00df181374fdd06c6a43298d98841e0953fe54
Christian S <velfundert@gmail.com>
2013-12-11 23:17:35 +0100

    rettet ut noen krøller
    render er nå et objekt det også
    flyttet selve fil-lesingskoden tilbake i nettsiden.

--- csv_parse.js
+++ csv_parse.js
@@ -1,52 +1,3 @@
-function startRead() {  
-  // obtain input element through DOM 
-  
-  var file = document.getElementById('filet').files[0];
-  if(file){
-    getAsText(file);
-  }
-}
-
-function getAsText(readFile) {
-        
-  var reader = new FileReader();
-  
-  // Read file into memory as UTF-8      
-  reader.readAsText(readFile, "UTF-8");
-  
-  // Handle progress, success, and errors
-  reader.onprogress = updateProgress;
-  reader.onload = loaded;
-  reader.onerror = errorHandler;
-}
-
-function updateProgress(evt) {
-  if (evt.lengthComputable) {
-    // evt.loaded and evt.total are ProgressEvent properties
-    var loaded = (evt.loaded / evt.total);
-    if (loaded < 1) {
-      // Increase the prog bar length
-      // style.width = (loaded * 200) + "px";
-    }
-  }
-}
-
-function loaded(evt) {  
-  // Obtain the read file data    
-  var fileString = evt.target.result;   
-
-  var p = new CSVparser;
-  p.parse( fileString );
-
-  var i = 0;
-  while ( i < p.data.length ) {
-      addInstitution( p.data[i][0], p.data[i][1] );
-      i++;
-  }
-  console.log("Done");
-
-}
-
 function CSVparser() {
 
     this.parse = parse;
@@ -101,11 +52,3 @@ function CSVparser() {
     }
 
 }
-
-function errorHandler(evt) {
-  if(evt.target.error.name == "NotReadableError") {
-    // The file could not be read
-  }
-}
-
-
--- render.js
+++ render.js
@@ -1,16 +1,39 @@
-function addField() {
-    addInstitution( "Studenter", "Institusjon");
+function FordelingRender() {
+
+    this.institutionCount = 0;
+
+    this.addField = addField;
+    this.addInstitution = addInstitution;
+    this.clear = clear;
+
+    function addField() {
+        this.addInstitution( "Studenter", "Institusjon");
+    }
+
+    function addInstitution( students, name ) {
+        var tf1 = document.createElement("input");
+        var tf2 = document.createElement("input");
+        var trg = document.getElementById("studentNumbers");
+        tf1.type="text";
+        tf2.type="text";
+        tf1.value = name;
+        tf2.value = students;
+        tf1.setAttribute("id", "i" + this.institutionCount );
+        tf2.setAttribute("id", "s" + this.institutionCount );
+        this.institutionCount++;
+        trg.appendChild( document.createElement("br") );
+        trg.appendChild( tf1 );
+        trg.appendChild( tf2 );
+    }
+
+    function clear() {
+       var trg = document.getElementById("studentNumbers");
+       while (trg.lastChild) {
+           trg.removeChild( trg.lastChild );
+       }
+       this.institutionCount = 0;
+    }
+
+
 }
 
-function addInstitution( students, name ) {
-    var tf1 = document.createElement("input");
-    var tf2 = document.createElement("input");
-    var trg = document.getElementById("studentNumbers");
-    tf1.type="text";
-    tf2.type="text";
-    tf1.value = name;
-    tf2.value = students;
-    trg.appendChild( document.createElement("br") );
-    trg.appendChild( tf1 );
-    trg.appendChild( tf2 );
-}
