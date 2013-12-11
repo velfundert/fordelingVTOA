c890599ca4784ee6a10d6854a52df2df259ec1a2
Christian S <velfundert@gmail.com>
2013-12-11 19:45:48 +0100

    better handling of nonstandard csv

--- csv_parse.js
+++ csv_parse.js
@@ -78,16 +78,21 @@ function CSVparser() {
     }
 
     function findSep( line ) {
-        var i = line.indexOf(",");
-        if ( i != -1 ) return;
-
-        i = line.indexOf(";");
+        // start with ; for silly excel
+        var i = line.indexOf(";");
 
         if ( i != -1 ) {
             this.sep = ";";
             console.log("separator is ; which isn't really the standard is it");
+            i = line.indexOf(",");
+            if ( i != -1 ) {
+                console.log("someone has put , in the integers or something!!");
+            }
             return;
         }
+
+        i = line.indexOf(",");
+        if ( i != -1 ) return;
 
         console.log("could not determine separator!");
     }
